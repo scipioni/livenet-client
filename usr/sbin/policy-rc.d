d1a056459b91a39d09df66a04bdb3192cfa8dc1b
Stefano Scipioni <scipio.it@gmail.com>
2017-01-30 10:55:50 +0100

    trusty

--- usr/sbin/policy-rc.d
+++ usr/sbin/policy-rc.d
@@ -2,6 +2,6 @@
 
 # dpkg/apt not start any daemons when things are installed in the chroot
 
-if [ ! "\`cat /etc/hostname\`" = "\`hostname\`" ]; then
+if [ ! "`cat /etc/hostname`" = "`hostname`" ]; then
     exit 101
 fi
